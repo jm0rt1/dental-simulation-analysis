3d7c06e4d5b76341e425a5c386b0a2ee2bfac3c7
Jamie Mortensen <16mortensenj@gmail.com>
2023-09-15 19:36:50 -0400

    added comma removal

--- src/app/analysis/idr/cash_management.py
+++ src/app/analysis/idr/cash_management.py
@@ -16,27 +16,29 @@ class CashManagement(ParserInterfaceable):
     @classmethod
     def from_lines_list_old(cls, lines_list: list[str]):
         cash_management = cls(
-            ending_cash_last_quarter=float(lines_list[86]),
-            ending_cash_this_quarter=float(lines_list[87]),
-            overdrawn_checks_paid=float(lines_list[88]),
-            profit_loss=float(lines_list[89]),
-            depreciation_expense=float(lines_list[90]),
-            loan_addition=float(lines_list[91]),
-            normal_loan_principal_payment=float(lines_list[92]),
-            draw=float(lines_list[93])
+            ending_cash_last_quarter=float(lines_list[86].replace(",", "")),
+            ending_cash_this_quarter=float(lines_list[87].replace(",", "")),
+            overdrawn_checks_paid=float(lines_list[88].replace(",", "")),
+            profit_loss=float(lines_list[89].replace(",", "")),
+            depreciation_expense=float(lines_list[90].replace(",", "")),
+            loan_addition=float(lines_list[91].replace(",", "")),
+            normal_loan_principal_payment=float(
+                lines_list[92].replace(",", "")),
+            draw=float(lines_list[93].replace(",", ""))
         )
         return cash_management
 
     @classmethod
     def from_lines_list_new(cls, lines_list: list[str]):
         cash_management = cls(
-            ending_cash_last_quarter=float(lines_list[86]),
-            ending_cash_this_quarter=float(lines_list[87]),
-            overdrawn_checks_paid=float(lines_list[88]),
-            profit_loss=float(lines_list[89]),
-            depreciation_expense=float(lines_list[90]),
-            loan_addition=float(lines_list[91]),
-            normal_loan_principal_payment=float(lines_list[92]),
-            draw=float(lines_list[93])
+            ending_cash_last_quarter=float(lines_list[86].replace(",", "")),
+            ending_cash_this_quarter=float(lines_list[87].replace(",", "")),
+            overdrawn_checks_paid=float(lines_list[88].replace(",", "")),
+            profit_loss=float(lines_list[89].replace(",", "")),
+            depreciation_expense=float(lines_list[90].replace(",", "")),
+            loan_addition=float(lines_list[91].replace(",", "")),
+            normal_loan_principal_payment=float(
+                lines_list[92].replace(",", "")),
+            draw=float(lines_list[93].replace(",", ""))
         )
         return cash_management

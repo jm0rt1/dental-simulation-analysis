341c55f5d32eba292090531d00bf2276361e689a
Jamie Mortensen <16mortensenj@gmail.com>
2023-09-14 21:39:24 -0400

    more file output

--- src/app/analysis/parser/parser.py
+++ src/app/analysis/parser/parser.py
@@ -3,11 +3,13 @@ import re
 import PyPDF2
 
 from src.app.analysis.idr.income_expense_data import Expenses, FixedCosts, Income, IncomeExpenseData, Marketing, Office, Other, Space, Staff, VariableCosts
-
+from src.shared.settings.settings import GlobalSettings
 # Function to extract text from the PDF
 
 
 class Parser():
+    PARSER_OUTPUT_DIR = GlobalSettings.OUTPUT_DIR/"parser"
+
     def __init__(self, pdf_path: Path):
         self.pdf_path = pdf_path
         if self.pdf_path.suffix != ".pdf":
@@ -19,8 +21,16 @@ class Parser():
 
     @property
     def report_text_file_path(self) -> Path:
-        file_path = Path(f"outputs/report_text_files/{self.pdf_path.stem}.txt")
-        file_path.mkdir(parents=True, exist_ok=True)
+        file_path = self.PARSER_OUTPUT_DIR / \
+            f"/report_text_files/{self.pdf_path.stem}.txt"
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        return file_path
+
+    @property
+    def report_sections_file_path(self) -> Path:
+        file_path = self.PARSER_OUTPUT_DIR / \
+            f"report_sections/{self.pdf_path.stem}.txt"
+        file_path.parent.mkdir(parents=True, exist_ok=True)
         return file_path
 
     def extract_text_from_pdf(self, pdf_path: Path) -> str:
@@ -42,14 +52,26 @@ class Parser():
 
         # Split the report into sections
         self.sections = re.split(r'\*\*\*', self.report_text)
+
+        # write report sections to a file in outputs/report_sections/{pdf_name}.txt
+
+        with open(self.report_sections_file_path, "w") as file:
+            file.write("\n\n".join(self.sections))
+
+        # Extract the INCOME AND EXPENSE STATEMENT section
+
         # write
         # Extract the INCOME AND EXPENSE STATEMENT section
         income_expense_section = self.sections[12]
 
+        # split the section into lines
         lines_list = income_expense_section.split("\n")
-        lines_list = [item.replace(
-            ",", "") for item in lines_list]
+
+        # lines_list = income_expense_section.split("\n")
+        # lines_list = [item.replace(
+        #     ",", "") for item in lines_list]
 
         # Now we'll populate our dataclass instances:
 
-       # self.data = IncomeExpenseData.from_lines_list(lines_list=lines_list)
+        self.data = IncomeExpenseData.from_lines_list_old(
+            lines_list=lines_list)

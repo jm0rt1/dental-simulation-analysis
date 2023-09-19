0e53014f2c38b0429611b86c797afb64b674bb35
Jamie Mortensen <16mortensenj@gmail.com>
2023-09-18 21:38:23 -0400

    more charts stuff

--- src/app/views/charts.py
+++ src/app/views/charts.py
@@ -5,17 +5,16 @@ import matplotlib.pyplot as plt
 def create_burndown_chart(df: pd.DataFrame, starting_cash: float):
     # Calculate net income
     # Adjust these column names based on your DataFrame
-    df['net_income'] = df['income_total'] - df['expenses_total']
 
     # Calculate cumulative net income
-    df['cumulative_net_income'] = df['net_income'].cumsum()
+    df['cumulative_net_income'] = df['income_expense_data*profit_loss_this_qtr'].cumsum()
 
     # Calculate cash balance
-    df['cash_balance'] = starting_cash + df['cumulative_net_income']
+    # df['cash_management_data*ending_cash_this_quarter'] = starting_cash + df['cumulative_net_income']
 
     # Plotting
     plt.figure(figsize=(10, 6))  # type:ignore
-    plt.plot(df['cash_balance'], label='Cash Balance',  # type:ignore
+    plt.plot(df['cash_management_data*ending_cash_this_quarter'], label='Cash Balance',  # type:ignore
              color='red')  # type:ignore
     # Line indicating zero cash balance #type:ignore
     plt.axhline(0, color='black', linestyle='--')  # type:ignore
@@ -26,3 +25,53 @@ def create_burndown_chart(df: pd.DataFrame, starting_cash: float):
     plt.grid(True)  # type:ignore
     plt.tight_layout()  # type:ignore
     plt.show()  # type:ignore
+
+
+def project_burndown_chart(df: pd.DataFrame, starting_cash: float = 0, projection_months=12):
+
+    # Calculate average monthly net income
+    avg_monthly_net_income = df['income_expense_data*profit_loss_this_qtr'].mean()
+
+    # Project future net incomes
+    future_net_incomes = [avg_monthly_net_income] * projection_months
+    future_dates = pd.date_range(
+        start=df.index[-1] + pd.DateOffset(1), periods=projection_months, freq='Q')
+
+    future_df = pd.DataFrame({
+        'income_expense_data*profit_loss_this_qtr': future_net_incomes,
+    }, index=future_dates)
+
+    # Combine historical and projected data
+    combined_df = pd.concat([df, future_df], axis=0)
+
+    # Calculate cumulative net income
+    combined_df['cumulative_net_income'] = combined_df['income_expense_data*profit_loss_this_qtr'].cumsum()
+
+    # Calculate cash balance
+    combined_df['cash_management_data*ending_cash_this_quarter'] = starting_cash + \
+        combined_df['cumulative_net_income']
+
+    # Plotting
+    plt.figure(figsize=(12, 7))
+    plt.plot(combined_df['cash_management_data*ending_cash_this_quarter'],
+             label='Cash Balance', color='red')
+    # Line indicating zero cash balance
+    plt.axhline(0, color='black', linestyle='--')
+    plt.title('Projected Burn-down Chart')
+    plt.xlabel('Time')
+    plt.ylabel('Cash Balance')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    # Find out when cash runs out
+    negative_cash = combined_df[combined_df['cash_management_data*ending_cash_this_quarter'] <= 0]
+    if not negative_cash.empty:
+        print(f"Projected to run out of money by: {negative_cash.index[0]}")
+    else:
+        print("Based on current projections, the company will not run out of money in the next year.")
+
+# Example usage:
+# df should be indexed by date for this to work
+# project_burndown_chart(df, starting_cash=100000)  # Replace 100000 with your actual starting cash
--- tests/analysis/views/test_charts.py
+++ tests/analysis/views/test_charts.py
@@ -1,9 +1,16 @@
+from pathlib import Path
 from src.app.analysis.parser.parser import Parser
+from src.app.analysis.idr.data_classes.report import Report, reports_to_dataframe
+from src.app.views.charts import project_burndown_chart
 import unittest
 
 
 class TestCharts(unittest.TestCase):
 
     def test_burndown_chart(self):
-        result = Parser("tests/test_files/Report_Q-9.pdf").parse()
-        
+        result0 = Parser(Path("tests/test_files/Report_Q-1-3.pdf")).parse()
+        result1 = Parser(Path("tests/test_files/Report_Q-2-3.pdf")).parse()
+
+        df = reports_to_dataframe([result0, result1])
+        print(df)
+        project_burndown_chart(df)
